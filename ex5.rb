22133d1639b3eafd6460914808bb22289bea4f77
Justin Walrath <walrathjaw@gmail.com>
2015-03-24 23:58:07 -0500

    added 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Justin Walrath'
+my_age = 24
+my_height = 10000 # Inches
+my_weight = 1000000 # lbs
+my_eyes = 'Blue'
+my_teeth = 'Red with the blood of my enemies'
+my_hair = 'Dirty Blonde'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actuallly that's huuuuuuuge"
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
